8dee2c6565061ad5d24396d9b96527ef9a78d454
Yaşar Özyurt <yasar.ozyurt@bilgeadam.com>
2023-07-28 10:52:41 +0300

    fix:Bug fix.

--- XoxoDay/__init__.py
+++ XoxoDay/__init__.py
@@ -2,7 +2,7 @@
 XoxoDay python library.
 """
 
-__version__ = "0.0.30"
+__version__ = "0.0.31"
 
 from XoxoDay.service.xoxoday_service import XoxoDayService
 from XoxoDay.exception import XoxoDayException
